773beba7a8f087854fbe0108c3493f356a5b8354
jhsoft8705 <jhonalexvillaflores@gmail.com>
2022-05-05 16:42:47 -0500

    agredo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,309 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    pagina1
-</body>
-</html>
+<html lang="en"><head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+
+    <title>LOREM NAME</title>
+  </head>
+  <body data-spy="scroll" data-target="#navbarSupportedContent" data-offset="57">
+    <!-- Header -->
+    <header class="sticky-top">
+      <nav id="header" class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src="images/platzi-logo.png" alt="Logo">
+            Why do we use it?
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link text-center active" href="#main">Why do we use it?</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-center" href="#speakers">Why do we use it?</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-center" href="#place-time">Why do we use it?</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-center" href="#conviertete-en-orador">Why do we use it?</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-center text-platzi" href="#" data-toggle="modal" data-target="#modalCompra">Compra tickets</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <!-- /Header -->
+
+    <!-- Main -->
+    <main id="main">
+      <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel" data-pause="false">
+        <div class="carousel-inner">
+          <div class="carousel-item">
+            <img class="d-block w-100" src="images/hawaii.jpg" alt="Hawaii 1">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="images/hawaii2.jpg" alt="Hawaii 2">
+          </div>
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="images/hawaii3.jpeg" alt="Hawaii 3">
+          </div>
+          <div class="overlay">
+            <div class="container">
+              <div class="row align-items-center">
+                <div class="col-md-6 offset-md-6 text-center text-md-right">
+                  <h1>Platzi Conf Hawaii</h1>
+                  <p class="d-none d-md-block">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                   when an unknown printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but also the leap into
+                  electronic typesetting, remaining essentially unchanged. It was popularised 
+                  in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                   and more recently with desktop publishing software like Aldus PageMaker including
+                   versions of Lorem Ipsum.
+                  </p>
+                  <a href="#" class="btn btn-outline-light">use it?</a>
+                  <button type="button" class="btn btn-platzi" data-toggle="modal" data-target="#modalCompra">Comprar tickets</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    <!-- /Main -->
+
+    <!-- speakers -->
+    <section id="speakers" class="mt-4">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center text-uppercase">
+            <small>use it?Conoce a los</small>
+            <h2>use it?</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 col-md-6 col-lg-3 mb-4">
+            <div class="card">
+              <img class="card-img-top" src="speakers/sacha.jpg" alt="Foto de Sacha">
+                <div class="card-body">
+                  <h5 class="card-title text-center">Sacha Lifszyc</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <div class="badges">
+                    <span class="badge badge-platzi mb-4">
+                    use it?
+                    </span>
+                  </div>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3 mb-4">
+            <div class="card">
+              <img class="card-img-top" src="speakers/leonidas2.jpeg" alt="Foto de Leonidas">
+                <div class="card-body">
+                  <h5 class="card-title text-center">use it?</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <div class="badges">
+                    <span class="badge badge-platzi mb-4">
+                      Desarrollo Web
+                    </span>
+                  </div>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3 mb-4">
+            <div class="card">
+              <img class="card-img-top" src="speakers/cvander.jpeg" alt="Foto de Christian">
+                <div class="card-body">
+                  <h5 class="card-title text-center">use it?.</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <div class="badges">
+                    <span class="badge badge-platzi mb-4">
+                      Startups
+                    </span>
+                  </div>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3 mb-4">
+            <div class="card">
+              <img class="card-img-top" src="speakers/freddy.jpeg" alt="Foto de Freddy">
+                <div class="card-body">
+                  <h5 class="card-title text-center">use it?</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <div class="badges">
+                    <span class="badge badge-platzi mb-4">
+                      IoT
+                    </span>
+                  </div>
+                  <a href="#" class="btn btn-primary">use it?</a>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /speakers -->
+
+    <!-- Lugar y fecha -->
+    <section id="place-time">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12 col-lg-6 pl-0 pr-0">
+            <img src="images/honolulu.jpg" alt="Foto de Honolulu">
+          </div>
+          <div class="col-12 col-lg-6 pt-4 pb-4">
+            <h2>Honolulu - Octubre 2022</h2>
+            <p>
+            The standard chunk of Lorem Ipsum used since the 1500s is 
+            reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by
+            English versions from the 1914 translation by H. Rackham.
+            </p>
+            <a href="https://es.wikipedia.org/wiki/Honolulu" class="btn btn-outline-light">Conoce más</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /Lugar y fecha -->
+
+    <!-- Conviertete en orador -->
+    <section id="conviertete-en-orador" class="pt-2 pb-2">
+      <div class="container">
+        <div class="row">
+          <div class="col text-uppercase text-center">
+            <small>
+              Conviertete en un
+            </small>
+            <h2>
+              Orador
+            </h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col text-center">
+            Anótate como un orador para dar una <abbr data-toggle="tooltip" title="" data-original-title="Charla de 5 minutos">charla ignite</abbr>.
+            Cuentanos de que quieres hablar!
+          </div>
+        </div>
+        <div class="row">
+          <div class="col col-md-10 offset-md-1 col-lg-8 offset-lg-2 pt-2 pb-2">
+            <form>
+              <div class="form-row">
+                <div class="form-group col-12 col-md-6">
+                  <input type="text" class="form-control" placeholder="Nombre">
+                </div>
+                <div class="form-group col-12 col-md-6">
+                  <input type="text" class="form-control" placeholder="Apellido">
+                </div>
+              </div>
+              <div class="form-row">
+                <div class="form-group col">
+                  <textarea name="text" class="form-control form-control-lg" placeholder="Sobre que quieres hablar?"></textarea>
+                  <small class="form-text form-muted">
+                    Recuerda incluir un titulo para tu charla
+                  </small>
+                </div>
+              </div>
+              <div class="form-row">
+                <div class="col">
+                  <button type="button" class="btn btn-platzi btn-block">
+                    Eviar
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /Conviertete en orador -->
+
+    <!-- Footer -->
+    <footer id="footer" class="pb-4 pt-4">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-12 col-lg">
+            <a href="#">Preguntas frecuentes</a>
+          </div>
+          <div class="col-12 col-lg">
+            <a href="#">Contactenos</a>
+          </div>
+          <div class="col-12 col-lg">
+            <a href="#">Lorem</a>
+          </div>
+          <div class="col-12 col-lg">
+            <a href="#">Lorem</a>
+          </div>
+          <div class="col-12 col-lg">
+            <a href="#">Terminos y condiciones</a>
+          </div>
+          <div class="col-12 col-lg">
+            <a href="#">Privacidad</a>
+          </div>
+          <div class="col-12 col-lg">
+            <a href="#">Estudiantes</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- /Footer -->
+
+    <!-- Modal -->
+    <div class="modal fade" id="modalCompra" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalCenterTitle">Comprar tickets</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-row">
+                <div class="form-group col">
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text" id="basic-addon1">@</span>
+                    </div>
+                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                  </div>
+                </div>
+              </div>
+            </form>
+            <div class="alert alert-success" role="alert">
+              Recibiras un correo una vez sea verificada tu compra!
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+            <button type="button" class="btn btn-platzi">Comprar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /Modal -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <script src="index.js"></script>
+  
+</body></html>
